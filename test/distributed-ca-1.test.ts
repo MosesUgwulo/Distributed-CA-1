1b82d67eb5516c38a23656c58832f8bfc9ef785e
MosesUgwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-11-12 13:03:36 +0000

    Initial commit

---
+++ test/distributed-ca-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistributedCa1 from '../lib/distributed-ca-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/distributed-ca-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistributedCa1.DistributedCa1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
