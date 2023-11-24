edb92b62d4a06ad5fd40b6bb56db2b13b6b45b2a
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-11-24 14:54:34 +0000

    Added validation to addReview endpoint

--- lambdas/addReview.ts
+++ lambdas/addReview.ts
@@ -1,7 +1,11 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { createDDbDocClient } from "../shared/util";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
 
+const ajv = new Ajv();
+const isValidBodyParams = ajv.compile(schema.definitions["Review"] || {})
 const ddbDocClient = createDDbDocClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
@@ -17,7 +21,20 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
         }
     }
 
-    const commandOutput = await ddbDocClient.send(new PutCommand({
+    if(!isValidBodyParams(body)) {
+        return {
+            statusCode: 500,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                message: "Invalid type. Must match Review schema",
+                schema: schema.definitions["Review"]
+            })
+        }
+    }
+
+    await ddbDocClient.send(new PutCommand({
         TableName: process.env.TABLE_NAME,
         Item: body
     }));
