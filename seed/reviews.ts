a35493d711b0f51ff2f53a3894b5a309b07929d8
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-11-26 10:24:19 +0000

    added get review by year endpoint

--- seed/reviews.ts
+++ seed/reviews.ts
@@ -7,7 +7,7 @@ export const review : Review[] = [
     reviewerName: "Geronimo1967",
     rating: 6,
     content: "To be honest, there's very little new in this formulaic action adventure. It's the usual old who has got the launch codes mystery that sees Sly Stallone's \"Barney\" recruit his best mate \"Christmas\" (Jason Statham) and their lethal gang of expendables to thwart the ambitions of an unknown criminal mastermind who is hoping to arm a boat load of nuclear missiles and start WWIII. Thing is, tragedy strikes very early on in the film and \"Christmas\" is taken off the team that is sent to avenge that. Is he staying home to fester? Of course not, and lucky too - for soon he is trying to free his pals from captivity before all hell breaks loose. Plenty of pyrotechnics, a bit of contrived sexual chemistry (with Megan Fox's \"Gina\") and loads of bullets and missiles flying around keep the film moving along well enough, but the story is the thinnest of the thin; the baddie's identity is hardly a challenge to discover nor is the twist in the tale at the end remotely unexpected. Good to see that the acting colossus that is Dolph Lundgren is still getting on screen nowadays, but otherwise this really is unremarkable stuff that even the usually charismatic Statham can't really sustain.",
-    reviewDate: "08/10/2023",
+    reviewDate: "2023/08/10",
   },
 
   {
@@ -15,7 +15,7 @@ export const review : Review[] = [
     reviewerName: "msbreviews",
     rating: 8,
     content: "An exhilarating experience! The action sequences were intense, and the plot kept me on the edge of my seat. Kudos to the entire cast for delivering a thrilling performance.",
-    reviewDate: "01/15/2023"
+    reviewDate: "2023/01/15"
   },
 
   {
@@ -23,7 +23,7 @@ export const review : Review[] = [
     reviewerName: "MovieBuff101",
     rating: 4,
     content: "Disappointing. The storyline felt forced, and the characters lacked depth. A missed opportunity for a great action film.",
-    reviewDate: "02/22/2023"
+    reviewDate: "2023/02/22"
   },
 
   {
@@ -31,7 +31,7 @@ export const review : Review[] = [
     reviewerName: "Cinephile22",
     rating: 7,
     content: "Solid entertainment! The chemistry between the characters added charm to the film. Some predictable moments, but overall a good watch.",
-    reviewDate: "03/05/2023"
+    reviewDate: "2023/03/05"
   },
 
   {
@@ -39,7 +39,7 @@ export const review : Review[] = [
     reviewerName: "FilmFanatic89",
     rating: 3,
     content: "Regrettably dull. The plot was uninspired, and the action scenes failed to impress. Not worth the hype.",
-    reviewDate: "11/19/2023"
+    reviewDate: "2023/11/19"
   },
 
   // 385687
@@ -48,7 +48,7 @@ export const review : Review[] = [
     reviewerName: "msbreviews",
     rating: 6,
     content: "MORE SPOILER-FREE MINI-REVIEWS @ https://www.msbreviews.com/movie-reviews/mini-reviews-2023-edition\r\n\r\n\"Fast X surprisingly pulls me back into a saga I had practically given up on. Jason Momoa is, by far, the MVP of the whole film, transforming an easily forgettable villain into a truly hilarious explosion of charm and *camp*. The cast still holds that enviable chemistry, even though Daniela Melchior and Portugal deserved more screen time.\r\n\r\nConsidering the previous entries, the energetic action actually turns out to be quite tolerable when it comes to the expected illogical insanity of the franchise - which will please viewers with less patience for absurdity. Despite an overuse of false backdrops and extremely agitated editing, the entertainment levels are well within the acceptable spectrum.\r\n\r\nIt's clearly the first film in a final trilogy. Interpret this last sentence as you wish.\"\r\n\r\nRating: B-",
-    reviewDate: "18/05/2023",
+    reviewDate: "2023/05/18",
   },
 
   {
@@ -56,7 +56,7 @@ export const review : Review[] = [
     reviewerName: "FilmFanatic89",
     rating: 6,
     content: "A decent movie with some interesting twists. While the plot was engaging, there were moments that felt a bit cliché. Overall, an enjoyable watch.",
-    reviewDate: "01/25/2023"
+    reviewDate: "2023/01/25"
   },
 
   {
@@ -64,7 +64,7 @@ export const review : Review[] = [
     reviewerName: "Cinephile22",
     rating: 5,
     content: "Mediocre at best. The film lacked originality, and the characters failed to leave a lasting impression. Not something I would recommend.",
-    reviewDate: "02/15/2023"
+    reviewDate: "2023/02/15"
   },
 
   {
@@ -72,7 +72,7 @@ export const review : Review[] = [
     reviewerName: "ActionAficionado",
     rating: 8,
     content: "Fantastic action sequences! The movie delivered on its promises, with a gripping storyline and well-executed stunts. A must-see for action fans.",
-    reviewDate: "03/10/2023"
+    reviewDate: "2023/03/10"
   },
 
   // 542198
@@ -81,7 +81,7 @@ export const review : Review[] = [
     reviewerName: "FilmBuff456",
     rating: 7,
     content: "A solid film with engaging storytelling. The characters were well-developed, and the plot kept me hooked from start to finish. Worth a watch!",
-    reviewDate: "04/05/2023"
+    reviewDate: "2023/04/05"
   },
 
   {
@@ -89,7 +89,7 @@ export const review : Review[] = [
     reviewerName: "Cinephile99",
     rating: 9,
     content: "Exceptional! This movie exceeded my expectations. Brilliant performances, a captivating plot, and breathtaking visuals. A true cinematic gem.",
-    reviewDate: "05/20/2023"
+    reviewDate: "2023/05/20"
   },
 
   {
@@ -97,7 +97,7 @@ export const review : Review[] = [
     reviewerName: "MovieManiac77",
     rating: 5,
     content: "Mixed feelings about this one. While some moments were entertaining, the overall story felt disjointed. Not a standout in its genre.",
-    reviewDate: "06/12/2023"
+    reviewDate: "2023/06/12"
   },
 
   // 789301
@@ -106,7 +106,7 @@ export const review : Review[] = [
     reviewerName: "FilmBuff456",
     rating: 8,
     content: "A cinematic masterpiece! The storytelling was compelling, and the characters were richly developed. This movie is a true work of art.",
-    reviewDate: "07/08/2023"
+    reviewDate: "2023/07/08"
   },
 
   {
@@ -114,7 +114,7 @@ export const review : Review[] = [
     reviewerName: "ActionAficionado",
     rating: 6,
     content: "A decent watch with moments of brilliance. The plot had potential, but some scenes felt a bit predictable. Overall, an enjoyable experience.",
-    reviewDate: "08/02/2023"
+    reviewDate: "2023/08/02"
   },
 
   // 654712
@@ -123,6 +123,6 @@ export const review : Review[] = [
     reviewerName: "Moses",
     rating: 7,
     content: "A well-executed film with strong performances. The storyline kept me engaged, and the characters were relatable. Recommended for a thoughtful movie night.",
-    reviewDate: "09/15/2023"
+    reviewDate: "2023/09/15"
   }
 ];
